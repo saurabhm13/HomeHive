e415477e9c7e55106e93a9c11636216ba08c0c91
Saurabh <saurabhm1309@gmail.com>
2024-05-18 10:26:03 +0530

    Project Setup

---
+++ app/src/main/java/com/example/homehive/ui/theme/Type.kt
@@ -0,0 +1,93 @@
+package com.example.homehive.ui.theme
+
+import androidx.compose.material3.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import com.example.homehive.R
+
+val ubuntu = FontFamily(
+    Font(R.font.ubuntu_light, FontWeight.Light),
+    Font(R.font.ubuntu_regular, FontWeight.Normal),
+    Font(R.font.ubuntu_bold, FontWeight.Bold),
+    Font(R.font.ubuntu_medium, FontWeight.Medium)
+
+)
+
+val Typography = Typography(
+
+    headlineLarge = TextStyle(
+        fontFamily = ubuntu,
+        fontSize = 40.sp,
+        fontWeight = FontWeight.Bold,
+    ),
+
+    headlineMedium = TextStyle(
+        fontFamily = ubuntu,
+        fontSize = 28.sp,
+        fontWeight = FontWeight.Normal,
+    ),
+
+    headlineSmall = TextStyle(
+        fontFamily = ubuntu,
+        fontSize = 24.sp,
+        fontWeight = FontWeight.Normal,
+    ),
+
+    labelLarge = TextStyle(
+        fontFamily = ubuntu,
+        fontSize = 14.sp,
+        fontWeight = FontWeight.Medium,
+    ),
+
+    labelMedium = TextStyle(
+        fontFamily = ubuntu,
+        fontSize = 12.sp,
+        fontWeight = FontWeight.Medium,
+    ),
+
+    labelSmall = TextStyle(
+        fontFamily = ubuntu,
+        fontSize = 11.sp,
+        fontWeight = FontWeight.Medium,
+    ),
+
+    titleLarge = TextStyle(
+        fontFamily = ubuntu,
+        fontSize = 22.sp,
+        fontWeight = FontWeight.Normal,
+    ),
+
+    titleMedium = TextStyle(
+        fontFamily = ubuntu,
+        fontSize = 16.sp,
+        fontWeight = FontWeight.Medium,
+    ),
+
+    titleSmall = TextStyle(
+        fontFamily = ubuntu,
+        fontSize = 14.sp,
+        fontWeight = FontWeight.Medium,
+    ),
+
+    bodyLarge = TextStyle(
+        fontFamily = ubuntu,
+        fontSize = 16.sp,
+        fontWeight = FontWeight.Normal,
+    ),
+
+    bodyMedium = TextStyle(
+        fontFamily = ubuntu,
+        fontSize = 14.sp,
+        fontWeight = FontWeight.Normal,
+    ),
+
+    bodySmall = TextStyle(
+        fontFamily = ubuntu,
+        fontSize = 12.sp,
+        fontWeight = FontWeight.Normal,
+    ),
+
+    )
